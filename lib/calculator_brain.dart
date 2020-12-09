f8f2c3d7ae31f174710d97e395b82c5ef27a9851
[Anima jain] <[anima.jain18@gmail.com]>
2020-12-09 18:31:30 +0530

    uploading project

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/cupertino.dart';
+import 'dart:math';
+
+class CalculatorBrain{
+  CalculatorBrain({@required this.height, @required this.weight});
+  final int height;
+  final int weight;
+  double _bmi;
+  String calculateBMI(){
+    _bmi = weight/pow(height/100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult(){
+    if(_bmi>=25){
+      return 'Overweight';
+    }else if(_bmi>18.5){
+      return 'Normal';
+    }else{
+      return 'Underweight';
+    }
+  }
+  String getInterpretation(){
+    if(_bmi>=25){
+      return 'You have higher than Normal Body Weight! Try to exercise more.';
+    }else if(_bmi>18.5){
+      return 'You have Normal Body Weight! Good Job.';
+    }else{
+      return 'You have lower than Normal Body Weight! Try to eat some more bites.';
+    }
+  }
+}
---
+++ lib/components/gender_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+class GenderCard extends StatelessWidget {
+  GenderCard({@required this.icon, @required this.text});
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon,
+          size:80.0,),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(text,
+          style: kLabelTextStyle,
+        ),
+      ],
+    );
+  }
+}
+
+
